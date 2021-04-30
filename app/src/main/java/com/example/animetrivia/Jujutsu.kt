4cc1683204c3b9b3c4e549367c72c95fb688ed17
boyinbluehues <therealraghavkashyap26@gmail.com>
2021-04-30 10:41:50 +0530

    working verified

---
+++ app/src/main/java/com/example/animetrivia/Jujutsu.kt
@@ -0,0 +1,6 @@
+package com.example.animetrivia
+
+data class Jujutsu(val option1:String,
+                   val option2:String,
+                   val option3:String,
+                   val option4:String)
